c11f586717f53baae3af708bbfccda41346a715c
merttsvnc <merttsvnc@gmail.com>
2023-10-14 20:51:30 +0900

    welcome page created

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("node login register");
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+// Route
+app.use("/", require("./routes/welcome"));
+app.use("/users", require("./routes/users"));
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    // await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
