b82607a7f377554b1d30103457016eef752a9e77
merttsvnc <merttsvnc@gmail.com>
2023-10-15 22:21:14 +0900

    added passport localStrategy

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const connectDB = require("./config/connectDB");
-const flash = require("connect-flash")
-const session = require("express-session")
+const flash = require("connect-flash");
+const session = require("express-session");
+
+// Passport config
+const passport = require("passport");
+require("./config/passport")(passport);
 
 // EJS
 app.use(expressLayouts);
@@ -14,21 +18,27 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
 // Express session
-app.use(session({
-  secret: "secret",
-  resave: true,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Connect flash
-app.use(flash())
+app.use(flash());
 
 // Global Vars
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash("success_msg")
-  res.locals.error_msg = req.flash("error_msg")
-  next()
-})
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
 
 // Route
 app.use("/", require("./routes/welcome"));
